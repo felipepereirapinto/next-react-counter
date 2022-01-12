a50674a84b235317fbd7a7e5e4fbda3a2c1c31d1
Felipe Pereira Pinto <ofelipepereira@gmail.com>
2022-01-12 17:58:00 -0300

    chore: refactoring

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,3 @@
-import { useState } from 'react'
-
-function Button({ onClick, children }) {
-  return <button onClick={onClick}>{children}</button>
-}
-
-function Counter() {
-  const [counter, setCounter] = useState(1)
-
-  function addCounter() {
-    setCounter(counter + 1)
-  }
-
-  function subCounter() {
-    setCounter(counter - 1)
-  }
-  
-  return (
-    <>
-      <h1>Counter</h1>
-      <div>{counter}</div>
-      <div>
-        <Button onClick={addCounter}>+</Button>
-        <Button onClick={subCounter}>-</Button>
-      </div>
-    </>
-  )
-}
-
 function Home() {
   return <Counter />
 }
