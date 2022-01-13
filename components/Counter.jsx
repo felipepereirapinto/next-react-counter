e22f919d9bd47aa912b93550512b707573fdc35a
Felipe Pereira Pinto <ofelipepereira@gmail.com>
2022-01-13 17:03:52 -0300

    chore: reset button

--- components/Counter.jsx
+++ components/Counter.jsx
@@ -12,13 +12,18 @@ function Counter() {
     setCounter(counter - 1)
   }
   
+  function resetCounter() {
+    setCounter(0)
+  }
+
   return (
     <>
       <h1>Counter</h1>
       <div>{counter}</div>
       <div>
-        <Button onClick={addCounter}>+</Button>
         <Button onClick={subCounter}>-</Button>
+        <Button onClick={resetCounter}>0</Button>
+        <Button onClick={addCounter}>+</Button>
       </div>
     </>
   )
