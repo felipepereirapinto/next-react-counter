0a953655f1d28ab8c44f74b38bd68c3e3a67b9bf
Felipe Pereira Pinto <ofelipepereira@gmail.com>
2022-01-14 14:42:04 -0300

    refactoring counter

--- components/Counter.tsx
+++ components/Counter.tsx
@@ -4,22 +4,14 @@ import Button from './Button'
 function Counter() {
   const [counter, setCounter] = useState(1)
 
-  function addCounter() {
-    setCounter(counter + 1)
-  }
-
-  function subCounter() {
-    setCounter(counter - 1)
-  }
-  
-  function resetCounter() {
-    setCounter(0)
-  }
+  const addCounter = () => setCounter(counter + 1)
+  const subCounter = () => setCounter(counter - 1)
+  const resetCounter = () => setCounter(0)
 
   return (
     <>
       <h1>Counter</h1>
-      <div>{counter}</div>
+      <p>{counter}</p>
       <div>
         <Button onClick={subCounter} icon={'-'} />
         <Button onClick={resetCounter} icon={'0'} />
