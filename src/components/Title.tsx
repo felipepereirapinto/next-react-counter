0ec22a1b0db56c9d8397eb4577e10bd2e47c7c49
Felipe Pereira Pinto <ofelipepereira@gmail.com>
2022-01-17 18:52:47 -0300

    chore: refactoring Title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,14 +1,17 @@
-import { ReactChildren } from "react"
-
 type TitleProps = {
-  children: ReactChildren
+  children: React.ReactChildren
 }
 
 const Title: React.FC = ({ children }: TitleProps) => {
   return (
-    <h1 className='text-center h1'>
-      {children}
-    </h1>
+    <a
+      href="https://github.com/felipepereirapinto/next-react-counter"
+      className="text-decoration-none text-dark"
+    >
+      <h1 className='text-center h1'>
+        {children}
+      </h1>
+    </a>
   )
 }
 
