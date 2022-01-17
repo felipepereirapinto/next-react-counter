e31c8198637c1d8ea7b85eaae5f4bc653c8bf4f5
Felipe Pereira Pinto <ofelipepereira@gmail.com>
2022-01-17 16:59:52 -0300

    chore: Counter refactoring

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
+
+import Container from './Container'
+import Title from './Title'
+import Display from './Display'
 import Button from './Button'
 
-function Counter() {
+const Counter: React.FC = () => {
   const [counter, setCounter] = useState(0)
 
   const addCounter = () => setCounter(counter + 1)
@@ -9,25 +13,16 @@ function Counter() {
   const resetCounter = () => setCounter(0)
 
   return (
-    <div 
-      className='vh-100 vw-100 d-flex flex-column justify-content-center align-items-center'
-    >
-      <h1 className='text-center h1'>Counter</h1>
+    <Container>
+      <Title>Counter</Title>
+      <Display>{counter}</Display>
 
-      <p 
-        className='text-center display-1 m-5'
-      >
-        {counter}
-      </p>
-      
-      <div 
-        className='row' 
-      >
-        <Button onClick={subCounter} icon={'-'} className='btn-danger col mx-1' />
-        <Button onClick={resetCounter} icon={'0'} className='btn-primary col mx-1' />
-        <Button onClick={addCounter} icon={'+'} className='btn-success col mx-1' />
+      <div>
+        <Button onClick={subCounter} icon={'-'} color='danger' />
+        <Button onClick={resetCounter} icon={'0'} color='primary' />
+        <Button onClick={addCounter} icon={'+'} color='success' />
       </div>
-    </div>
+    </Container>
   )
 }
 
